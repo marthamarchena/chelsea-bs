27162ecfd25d5a6f880547d7ec997ffeee6c1eaf
Martha Marchena <marchenamartha@gmail.com>
2022-08-01 00:20:03 -0300

    cards productos

--- js/index.js
+++ js/index.js
@@ -1,5 +1,34 @@
-// let boton = document.getElementById ("open");
-// boton.addEventListener("click", respuestaClick); 
-// function respuestaClick() {
-//     console.log("Carrito Abierto")
+// Inicio de Sesion
+let usuario;
+let usuarioStorage = localStorage.getItem("usuario"); 
+
+if (usuarioStorage){
+    let usuario = usuarioStorage
+    let mensaje = `Bienvenido de vuelta ${usuario}`;
+    alert(mensaje);
+}else{
+    usuario = prompt("Ingrese su nombre");
+    localStorage.setItem("usuario", usuario);
+}
+
+// Cerrar Sesion
+const cerrarSesion = document.getElementById("sesion");
+cerrarSesion.addEventListener("click", () =>
+    function cerrar(){
+        localStorage.removeItem("usuario");
+        console.log("Su sesión ha sido cerrada");
+    }
+)
+
+// Zona de ubicacion
+// let ubicacion;
+// let ubicacionStorage = localStorage.getItem("ubicacion"); 
+
+// if (usuarioStorage){
+//     let ubicacion 
+//     alert(mensaje);
+// }else{
+//     usuario = prompt("Ingrese su nombre");
+//     localStorage.setItem("usuario", usuario);
 // }
+
--- js/productos.js
+++ js/productos.js
@@ -1,9 +1,63 @@
-// const botoncarrito = document.querySelectorAll (`.cards`);
-// botoncarrito.forEach(cardsboton => {
-//     cardsboton.addEventListener(`click`, cardclick)
-// });
+const items = document.getElementById(`items`)
+const templateCard = document.getElementById(`template-card`).content
+const fragment = document.createDocumentFragment()
+let carrito = {}
 
-// function cardclick(event) {
-//     const boton = event.target;
+document.addEventListener(`DOMContentLoaded`, () => {
+    fetchData()
+})
+items.addEventListener(`click`, e =>{
+    addCarrito(e)
+} )
 
-// }
+const fetchData = async () => {
+    try{
+        const res = await fetch(`comidas.json`)
+        const data = await res.json()
+        // console.log(data)
+        pintarCards(data)
+    } catch (error){
+        console.log(error)
+    }
+}
+
+const pintarCards = data => {
+    data.forEach (producto => {
+        templateCard.querySelector(`h5`).textContent = producto.nombre
+        templateCard.querySelector(`h6`).textContent = producto.descripcion
+        templateCard.querySelector(`p`).textContent = producto.precio
+        // templateCard.querySelector(`img`).setAttibute("src", producto.img)
+        templateCard.querySelector(`.btn-light`).dataset.id = producto.id
+
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    items.appendChild(fragment)
+}
+
+const addCarrito = e => {
+    // console.log(e.target)
+    // console.log(e.target.classList.contains(`btn-light`));
+    if (e.target.classList.contains(`btn-light`)) {
+        setCarrito(e.target.parentElement); 
+    }
+    e.stopPropagation ()
+}
+
+const setCarrito = objeto => {
+    // console.log(objeto);
+    const producto = {
+        id: objeto.querySelector(`.btn-light`).dataset.id,
+        nombre: objeto.querySelector(`h5`).textContent,
+        precio: objeto.querySelector(`p`).textContent,
+        cantidad: 1
+    }
+
+    if (carrito.hasOwnProperty(producto.id)){
+        producto.cantidad = carrito[producto.id].cantidad + 1
+    }
+
+    carrito[producto.id] = {...producto}
+
+    console.log(producto);
+}
